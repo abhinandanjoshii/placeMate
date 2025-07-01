7be5f5149623903888b593d864a6e2967f83e75d
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-07-02 00:27:37 +0530

    async code for challenge-generator

--- frontend/src/challenge/ChallengeGenerator.jsx
+++ frontend/src/challenge/ChallengeGenerator.jsx
@@ -1,5 +1,23 @@
 import "react"
+import {useState, useEffect} from "react"
+import {MCQChallenge} from "./MCQChallenge.jsx"
 
 export function ChallengeGenerator(){
-    return <></>
+    const [challenges, setChallenges] = useState()
+    const [loading, setIsLoading] = useState(false)
+    const [error, setError] = useState(null)
+    const [difficulty, setDifficulty] = useState("easy")
+    const [quota, setQuota] = useState(null)
+
+    const fetchQuota = async () => {
+    }
+    const generateChallenge = async () => {}
+    const getNextResetTime = () => {}
+
+    return <div className="challenge-container">
+        <h2>Placement Challenge Generator</h2>
+
+        <div className="quota-display">
+            <p
+    </div>
 }
