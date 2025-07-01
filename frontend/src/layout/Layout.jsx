0432281dc7c07ca03da975025ff8619f57b18ff8
abhinandanjoshii <113920019+abhinandanjoshii@users.noreply.github.com>
2025-07-02 00:41:17 +0530

    updated frontend (not fixed)

--- frontend/src/layout/Layout.jsx
+++ frontend/src/layout/Layout.jsx
@@ -1,24 +1,25 @@
 import "react"
 import {SignedIn, SignedOut, UserButton} from "@clerk/clerk-react"
-import { Outlet, Link, Navigate} from "react-router-dom"
+import {Outlet, Link, Navigate} from "react-router-dom"
 
-export function Layout(){
+export function Layout() {
     return <div className="app-layout">
         <header className="app-header">
             <div className="header-content">
-                <h1 className="PlaceMate - Placement Question Generator"></h1>
+                <h1>Code Challenge Generator</h1>
                 <nav>
                     <SignedIn>
                         <Link to="/">Generate Challenge</Link>
                         <Link to="/history">History</Link>
-                        <UserButton />
+                        <UserButton/>
                     </SignedIn>
                 </nav>
             </div>
         </header>
+
         <main className="app-main">
             <SignedOut>
-                <Navigate to="/sign-in" replace />
+                <Navigate to="/sign-in" replace/>
             </SignedOut>
             <SignedIn>
                 <Outlet />
